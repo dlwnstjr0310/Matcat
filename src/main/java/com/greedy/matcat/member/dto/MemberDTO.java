89175ee686775053107e6cda20139192aa794484
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-03-13 15:36:47 +0900

    hello

---
+++ src/main/java/com/greedy/matcat/member/dto/MemberDTO.java
@@ -0,0 +1,15 @@
+package com.greedy.matcat.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberDTO {
+
+    private String memberNo;
+    private String memberId;
+    private String memberPwd;
+
+
+}
