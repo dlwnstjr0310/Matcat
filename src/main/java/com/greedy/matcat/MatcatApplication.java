c3101bf048abafd50422c9bd7a0942e4871d2b11
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-03-13 14:53:55 +0900

    first commit

---
+++ src/main/java/com/greedy/matcat/MatcatApplication.java
@@ -0,0 +1,13 @@
+package com.greedy.matcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatcatApplication.class, args);
+    }
+
+}
