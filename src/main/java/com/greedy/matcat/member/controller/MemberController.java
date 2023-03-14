07c5468c788b5f13ba691786dff35f4c1038562d
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-03-14 17:09:48 +0900

    비밀번호 찾기 로직 작성중

--- src/main/java/com/greedy/matcat/member/controller/MemberController.java
+++ src/main/java/com/greedy/matcat/member/controller/MemberController.java
@@ -1,12 +1,29 @@
 package com.greedy.matcat.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/member")
+@Transactional
 public class MemberController {
+
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Autowired
+    public MemberController(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
 
     @GetMapping("/login")
     public void login() {
@@ -17,12 +34,19 @@ public class MemberController {
     }
 
     @GetMapping("/regist2")
-    public void regist2(){}
+    public void regist2() {
+    }
 
     @GetMapping("/findId")
     public void findId() {
     }
 
     @GetMapping("/findPWD")
-    public void findPWD(){}
+    public void findPWD() {
+    }
+
+    @PostMapping("findPWD")
+    public String sendMail(){
+        return "";
+    }
 }
