a01b97507ab5128734b09f23c13a83c2ebb6df65
KJlee <lkj9676@gmail.com>
2023-03-15 18:11:12 +0900

    css 수정

--- src/main/java/com/greedy/matcat/admin/controller/AdminController.java
+++ src/main/java/com/greedy/matcat/admin/controller/AdminController.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/admin")
 public class AdminController {
 
-	@GetMapping("/adminHelp001")
+	@GetMapping("adminHelp001")
 	public void adminHelp001() {}
 	
 	@GetMapping("adminHelp002")
